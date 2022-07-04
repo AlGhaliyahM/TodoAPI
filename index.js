0bc28b574dacfb86cd5288eae57fe15d16bb8825
AlGhaliyahM <439201044@student.ksu.edu.sa>
2022-07-05 00:59:37 +0300

    Update

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 var express = require("express");
 var app = express();
 const PORT = process.env.PORT || 3000;
+const { Todo } = require("./Todo");
+
 app.get("/", (req, res) => {
   res.send("This is my demo project");
 });
 app.listen(PORT, function () {
-  console.log(`Demo project at: ${PORT}!`);
+  console.log(`Todo api at: ${PORT}!`);
 });
