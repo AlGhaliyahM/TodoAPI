c3bd12fd804bf0866db6e9d6302748f983d3caf7
AlGhaliyahM <439201044@student.ksu.edu.sa>
2022-08-01 19:57:41 +0300

    Added the roadmap

--- src/todo/todo.service.ts
+++ src/todo/todo.service.ts
@@ -13,11 +13,13 @@ export class TodoService {
     private TodoRepository: Repository<Todo>,
   ) {}
 
+  // DONE  But response is missing
   async postTask(todo: Todo) {
     const task = await this.TodoRepository.save(todo);
     // return task;
   }
 
+  // DONE  But response is missing
   async getTaskByID(ID) {
     const task = await this.TodoRepository.findOneBy({
       id: ID,
@@ -25,10 +27,12 @@ export class TodoService {
     console.log(task);
   }
 
+  //There is a Problem
   async getAllTask() {
     return this.TodoRepository.find({});
   }
 
+  // DONE  But response is missing
   async deleteTaskByID(ID) {
     const taskToDelete = await this.TodoRepository.findOneBy({
       id: ID,
@@ -37,6 +41,7 @@ export class TodoService {
     await this.TodoRepository.remove(taskToDelete);
   }
 
+  //Date need to be uodated + (isdone)
   async updateTask(ID, todo: Todo) {
     const task = await this.TodoRepository.findOneBy({
       id: ID,
