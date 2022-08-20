957e099e1c4b49c2f862d8f5482f82b49254ebf7
AlGhaliyahM <439201044@student.ksu.edu.sa>
2022-08-20 20:24:12 +0300

    User Signup logic

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -15,7 +15,20 @@ import { Request, Response } from 'express';
 import { User } from './user.entity';
 import { UsersService } from './user.service';
 
+import jwt from 'jsonwebtoken';
+
 @Controller('user')
 export class UserController {
   constructor(private usersService: UsersService) {}
+
+  //   @Post('signin')
+  //   async signIn() {
+  //     return this.usersService.signIn();
+  //   }
+
+  @Post('signup')
+  async signUp(@Body() user: User): Promise<User> {
+    const test = await this.usersService.signUp(user);
+    return test;
+  }
 }
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,7 +1,9 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, Post, Get, Req, Res, Body, Delete } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { User } from './user.entity';
+import { Request, Response } from 'express';
+import jwt from 'jsonwebtoken';
 
 @Injectable()
 export class UsersService {
@@ -9,4 +11,18 @@ export class UsersService {
     @InjectRepository(User)
     private usersRepository: Repository<User>,
   ) {}
+
+  //   async signIn() {}
+
+  async signUp(user: User) {
+    const argon2 = require('argon2');
+
+    const hash = await argon2.hash(user.password);
+
+    return await this.usersRepository.save({
+      name: user.name,
+      password: hash,
+      email: user.email,
+    });
+  }
 }
